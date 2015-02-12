01e27283fd1860aae279b73b479eaa9e6fef0e4b
n@ml.org <n@ml.org@f12b1da6-ca34-0410-bd5f-8d6511b6bbc3>
2015-02-12 01:08:08 +0000

    Initial commit, just the bare bones for rolling a character.

---
+++ trunk/web/rpg/abilityscores.js
@@ -0,0 +1,54 @@
+/**
+ * @fileoverview http://www.opengamingfoundation.org/srd/srdabilityscores.rtf
+ * @author Daniel Reed &lt;<a href="mailto:n@ml.org">n@ml.org</a>&gt;
+ */
+
+(function() {
+
+nmlorg.require('nmlorg.rpg.basics');
+
+/** @namespace */
+nmlorg.rpg.abilityscores = nmlorg.rpg.abilityscores || {};
+
+
+nmlorg.rpg.abilityscores.ABILITIES = {
+    'strength': 0, 'dexterity': 0, 'constitution': 0, 'intelligence': 0, 'wisdom': 0, 'charisma': 0};
+
+
+nmlorg.rpg.abilityscores.AbilityScores = function() {};
+
+
+for (var k in nmlorg.rpg.abilityscores.ABILITIES) (function() {
+  var ability = k;
+
+  nmlorg.rpg.abilityscores.AbilityScores.prototype[ability] = 0;
+
+  // Each ability will have a modifier. The modifier can be calculated using this formula:
+  //   (ability/2) -5 [round result down]
+  // The modifier is the number you add to or subtract from the die roll when your character tries
+  // to do something related to that ability. A positive modifier is called a bonus, and a negative
+  // modifier is called a penalty. 
+  Object.defineProperty(nmlorg.rpg.abilityscores.AbilityScores.prototype, ability + 'Modifier',
+                        {'configurable': false, 'enumerable': true,
+                         'get': function() { return Math.floor(this[ability] / 2) - 5; }});
+})();
+
+
+Object.defineProperty(nmlorg.rpg.abilityscores.AbilityScores.prototype, 'attackModifier',
+                      function() {
+  // A creature with no Strength score can't exert force, usually because it has no physical body or
+  // because it doesn't move. The creature automatically fails Strength checks. If the creature can
+  // attack, it applies its Dexterity modifier to its base attack instead of a Strength modifier.
+  return this.strength > 0 ? this.strengthModifier : this.dexterityModifier;
+});
+
+
+nmlorg.rpg.abilityscores.AbilityScores.prototype.reroll = function(dice) {
+  if (!dice)
+    dice = new nmlorg.rpg.basics.Dice();
+
+  for (var ability in nmlorg.rpg.abilityscores.ABILITIES)
+    this[ability] = dice.roll('3d6');
+};
+
+})();
