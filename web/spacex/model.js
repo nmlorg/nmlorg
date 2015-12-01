a983dc31543deee1be0da90cde234a09a2ddcb1f
Daniel Reed <n@ml.org>
2015-12-01 00:07:07 -0800

    Revert "Add a nose cone, and color the floor panels black instead of white."
    This reverts commit 3686cae05f1857d3f20d938d8c6f3459ecd9b460.

--- web/spacex/model.js
+++ web/spacex/model.js
@@ -9,54 +9,6 @@ spacex.model = spacex.model || {};
 
 
 spacex.model.drawVehicle = function(ctx, camera) {
-  ctx.fillStyle = 'rgba(0, 0, 0, .5)';
-  camera.drawPath(ctx, [
-      [-295, 50, -45],
-      [400, 50, -45],
-      [400, 50, 45],
-      [-295, 50, 45],
-  ]);
-  ctx.fill();
-
-  camera.drawPath(ctx, [
-    [-305, 50, -45],
-    [-305, 50, 45],
-    [-395, 5, 0],
-  ]);
-  ctx.fill();
-
-  ctx.fillStyle = 'rgba(255, 255, 255, .5)';
-  camera.drawPath(ctx, [
-      [-295, 45, -50],
-      [400, 45, -50],
-      [400, -45, -50],
-      [-295, -45, -50],
-  ]);
-  ctx.fill();
-
-  camera.drawPath(ctx, [
-      [-295, 45, 50],
-      [400, 45, 50],
-      [400, -45, 50],
-      [-295, -45, 50],
-  ]);
-  ctx.fill();
-
-  camera.drawPath(ctx, [
-      [-295, -50, -45],
-      [400, -50, -45],
-      [400, -50, 45],
-      [-295, -50, 45],
-  ]);
-  ctx.fill();
-
-  camera.drawPath(ctx, [
-    [-305, -50, -45],
-    [-305, -50, 45],
-    [-395, -5, 0],
-  ]);
-  ctx.fill();
-
   ctx.fillStyle = 'rgba(0, 255, 0, .5)';
   camera.drawPath(ctx, [
       [-100, 0, -50],
@@ -66,10 +18,36 @@ spacex.model.drawVehicle = function(ctx, camera) {
   ]);
   ctx.fill();
 
+  ctx.fillStyle = 'rgba(255, 255, 255, .5)';
   camera.drawPath(ctx, [
-    [-305, 45, -50],
-    [-305, -45, -50],
-    [-395, 0, -5],
+      [-400, 45, -50],
+      [400, 45, -50],
+      [400, -45, -50],
+      [-400, -45, -50],
+  ]);
+  ctx.fill();
+
+  camera.drawPath(ctx, [
+      [-400, 45, 50],
+      [400, 45, 50],
+      [400, -45, 50],
+      [-400, -45, 50],
+  ]);
+  ctx.fill();
+
+  camera.drawPath(ctx, [
+      [-400, 50, -45],
+      [400, 50, -45],
+      [400, 50, 45],
+      [-400, 50, 45],
+  ]);
+  ctx.fill();
+
+  camera.drawPath(ctx, [
+      [-400, -50, -45],
+      [400, -50, -45],
+      [400, -50, 45],
+      [-400, -50, 45],
   ]);
   ctx.fill();
 
@@ -79,13 +57,6 @@ spacex.model.drawVehicle = function(ctx, camera) {
       [100, 0, 50],
       [100, 0, 400],
       [-100, 0, 400],
-  ]);
-  ctx.fill();
-
-  camera.drawPath(ctx, [
-    [-305, 45, 50],
-    [-305, -45, 50],
-    [-395, 0, 5],
   ]);
   ctx.fill();
 };
