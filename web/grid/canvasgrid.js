f452d8a3b730a7e6e2646c3eb6c9fb75a6e84088
Daniel Reed <n@ml.org>
2016-01-16 19:41:21 -0800

    Separate the background, editing grid, and foreground into separate HTMLCanvasElements (with a fourth just for the UI).
    Make the editing grid only appear when the canvas is hovered over.

--- web/grid/canvasgrid.js
+++ web/grid/canvasgrid.js
@@ -8,13 +8,20 @@ var nmlorg = window.nmlorg = window.nmlorg || {};
  * @constructor
  */
 nmlorg.CanvasGrid = function() {
-  var canvas = this.canvas_ = document.createElement('canvas');
-  var ctx = this.ctx_ = canvas.getContext('2d');
-
+  this.body_ = document.createElement('div');
+  this.body_.className = 'canvas';
   this.cellWidth = this.cellHeight = 32;
   this.width = this.height = 20;
-  canvas.width = this.width * this.cellWidth;
-  canvas.height = this.height * this.cellHeight;
+  this.bgCanvas_ = this.makeCanvas();
+  this.bgCtx_ = this.bgCanvas_.getContext('2d');
+  this.gridCanvas_ = this.makeCanvas();
+  this.gridCanvas_.className = 'on-hover';
+  this.gridCtx_ = this.gridCanvas_.getContext('2d');
+  this.setGrid();
+  this.fgCanvas_ = this.makeCanvas();
+  this.fgCtx_ = this.fgCanvas_.getContext('2d');
+  this.uiCanvas_ = this.makeCanvas();
+  this.uiCtx_ = this.uiCanvas_.getContext('2d');
   this.cells_ = {};
 };
 
@@ -24,8 +31,8 @@ nmlorg.CanvasGrid = function() {
  * @param {HTMLElement} parent An element reachable at or from document.body.
  */
 nmlorg.CanvasGrid.prototype.attach = function(parent) {
-  parent.appendChild(this.canvas_);
-  this.canvas_.focus();
+  parent.appendChild(this.body_);
+  this.body_.focus();
   return this;
 };
 
@@ -34,30 +41,52 @@ nmlorg.CanvasGrid.prototype.attach = function(parent) {
  * Draw the canvas.
  */
 nmlorg.CanvasGrid.prototype.draw = function() {
-  var ctx = this.ctx_;
+  var ctx = this.fgCtx_;
 
-  ctx.fillStyle = 'rgba(0, 0, 0, .2)';
   for (var i = 0; i < this.width; i++) {
-    var x = i * this.cellWidth;
-
     for (var j = 0; j < this.height; j++) {
-      var y = j * this.cellHeight;
-
-      if (this.bg_)
-        this.bg_.draw(ctx, x, y, this.cellWidth, this.cellHeight);
-      if ((i + j) % 2)
-        ctx.fillRect(x, y, this.cellWidth, this.cellHeight);
       var tiles = this.cells_[j * this.width + i];
       if (tiles)
         for (var tile of tiles)
-          tile.draw(ctx, x, y, this.cellWidth, this.cellHeight);
+          tile.draw(ctx, i * this.cellWidth, j * this.cellHeight, this.cellWidth, this.cellHeight);
     }
   }
 };
 
 
+/**
+ * Create a canvas and add it to the rendering stack.
+ */
+nmlorg.CanvasGrid.prototype.makeCanvas = function() {
+  var canvas = document.createElement('canvas');
+  this.body_.appendChild(canvas);
+  canvas.width = this.width * this.cellWidth;
+  canvas.height = this.height * this.cellHeight;
+  return canvas;
+};
+
+
 nmlorg.CanvasGrid.prototype.setBackground = function(tile) {
-  this.bg_ = tile;
+  var ctx = this.bgCtx_;
+
+  for (var i = 0; i < this.width; i++)
+    for (var j = 0; j < this.height; j++)
+      tile.draw(ctx, i * this.cellWidth, j * this.cellHeight, this.cellWidth, this.cellHeight);
+};
+
+
+nmlorg.CanvasGrid.prototype.setGrid = function() {
+  var ctx = this.gridCtx_;
+
+  for (var i = 0; i < this.width; i++) {
+    for (var j = 0; j < this.height; j++) {
+      if ((i + j) % 2)
+        ctx.fillStyle = 'rgba(0, 0, 0, .1)';
+      else
+        ctx.fillStyle = 'rgba(255, 255, 255, .1)';
+      ctx.fillRect(i * this.cellWidth, j * this.cellHeight, this.cellWidth, this.cellHeight);
+    }
+  }
 };
 
 
