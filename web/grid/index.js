3e3d27af5f0d6ca7cdd16de84e0abd41fd54aeba
Daniel Reed <n@ml.org>
2016-02-06 18:27:16 -0800

    Add an example ladder icon too.

--- web/grid/index.js
+++ web/grid/index.js
@@ -11,13 +11,17 @@ window.addEventListener('load', function(e) {
   var sheet = new nmlorg.Sheet(img, 32, 32);
   var grass = sheet.getTile(10, 20);
   var trash = sheet.getTile(26, 27);
+  var ladder = sheet.getTile(6, 4);
 
   grid.setBackground(grass);
   grid.setForeground(1, 1, trash);
   grid.setForeground(2, 1, trash, trash);
   grid.setForeground(3, 1, trash, trash, trash);
-  grid.setForeground(2, 2, trash);
-  grid.setForeground(2, 3, trash);
+  grid.setForeground(4, 1, trash, ladder);
+  grid.setForeground(1, 2, ladder);
+  grid.setForeground(2, 2, ladder, ladder);
+  grid.setForeground(3, 2, ladder, ladder, ladder);
+  grid.setForeground(4, 2, ladder, trash);
 
   window.requestAnimationFrame(function anim() {
     grid.draw();
