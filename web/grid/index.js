df9eb88bde53ed5fdf523ae12a82a59b06e3fb9d
Daniel Reed <n@ml.org>
2016-02-07 19:41:17 -0800

    Add a gremlin and a plant.

--- web/grid/index.js
+++ web/grid/index.js
@@ -10,8 +10,10 @@ window.addEventListener('load', function(e) {
   img.src = 'hyptosis_tile-art-batch-1.png';
   var sheet = new nmlorg.Sheet(img, 32, 32);
   var grass = sheet.getTile(10, 20);
-  var trash = sheet.getTile(26, 27);
+  var gremlin = sheet.getTile(1, 26);
   var ladder = sheet.getTile(6, 4);
+  var plant = sheet.getTile(14, 15);
+  var trash = sheet.getTile(26, 27);
 
   grid.setBackground(grass);
   grid.setForeground(1, 1, trash);
@@ -22,6 +24,8 @@ window.addEventListener('load', function(e) {
   grid.setForeground(2, 2, ladder, ladder);
   grid.setForeground(3, 2, ladder, ladder, ladder);
   grid.setForeground(4, 2, ladder, trash);
+  grid.setForeground(1, 3, gremlin);
+  grid.setForeground(2, 3, plant);
 
   window.requestAnimationFrame(function anim() {
     grid.draw();
