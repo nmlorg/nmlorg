a48eb1c150e17db0f6ada2c366da2255dddb6dc5
Daniel Reed <n@ml.org>
2016-02-08 19:48:14 -0800

    Add a quick explosion animation.

--- web/grid/index.js
+++ web/grid/index.js
@@ -7,15 +7,22 @@ var nmlorg = window['nmlorg'] = window['nmlorg'] || {};
 window.addEventListener('load', function(e) {
   var grid = new nmlorg.Grid(20, 10).attach(document.body);
   var img = document.createElement('img');
-  img.src = 'hyptosis_tile-art-batch-1.png';
+  img.src = 'tmp/hyptosis_tile-art-batch-1.png';
   var sheet = new nmlorg.Sheet(img, 32, 32);
   var grass = sheet.getTile(10, 20);
   var gremlin = sheet.getTile(1, 26);
   var ladder = sheet.getTile(6, 4);
   var plant = sheet.getTile(14, 15);
   var trash = sheet.getTile(26, 27);
-  var silly = new nmlorg.AnimatedTile(3000, gremlin, trash);
-  var sillier = new nmlorg.AnimatedTile(2000, trash, plant, gremlin, ladder);
+  var silly = new nmlorg.AnimatedTile(1000, trash, ladder, gremlin, plant);
+  img = document.createElement('img');
+  img.src = 'tmp/BombExploding.png';
+  sheet = new nmlorg.Sheet(img, 32, 64);
+  var explosion = new nmlorg.AnimatedTile(
+      1000 / 10, sheet.getTile(0, 0), sheet.getTile(1, 0), sheet.getTile(2, 0), sheet.getTile(3, 0),
+      sheet.getTile(4, 0), sheet.getTile(5, 0), sheet.getTile(6, 0), sheet.getTile(7, 0),
+      sheet.getTile(8, 0), sheet.getTile(9, 0), sheet.getTile(10, 0), sheet.getTile(11, 0),
+      sheet.getTile(12, 0));
 
   grid.setBackground(grass);
   grid.setForeground(1, 1, trash);
@@ -29,7 +36,7 @@ window.addEventListener('load', function(e) {
   grid.setForeground(1, 3, gremlin);
   grid.setForeground(2, 3, plant);
   grid.setForeground(3, 3, silly);
-  grid.setForeground(4, 3, sillier);
+  grid.setForeground(4, 3, explosion);
 
   window.requestAnimationFrame(function anim() {
     grid.draw();
