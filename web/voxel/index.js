3444b40cbe5df78b6376e5fe2e413615b38e6391
Daniel Reed <n@ml.org>
2016-03-15 21:07:28 -0700

    Switch from matrix-based camera movement to yaw/pitch/roll. (Our motion forward doesn't depend on what direction we're facing up/down--we always move at the same speed along the ground plane.)

--- web/voxel/index.js
+++ web/voxel/index.js
@@ -4,6 +4,12 @@
 var nmlorg = window['nmlorg'] = window['nmlorg'] || {};
 
 
+var mouseSensitivity = .25;
+var forwardPerSec = 5;
+var backPerSec = 2;
+var strafePerSec = 3;
+
+
 window.addEventListener('load', function(e) {
   var viewportDiv = document.createElement('div');
   document.body.appendChild(viewportDiv);
@@ -19,11 +25,18 @@ window.addEventListener('load', function(e) {
   escapedBoxDiv.classList.add('box');
   escapedBoxDiv.textContent = 'Press Esc to toggle between navigation and menu mode.';
 
-  var camera = new nmlorg.Camera();
+  var pos = [0, 0, 0];
+  var yaw = 0, pitch = 0, roll = 0;
 
   function mouseMove(e) {
-    camera.rotateY(-e.movementX);
-    camera.rotateX(-e.movementY);
+    yaw += deg2rad(e.movementX * mouseSensitivity);
+    while (yaw < 0)
+      yaw += 2 * Math.PI;
+    while (yaw >= 2 * Math.PI)
+      yaw -= 2 * Math.PI;
+
+    pitch = Math.max(deg2rad(-85), Math.min(deg2rad(85),
+                                            pitch + deg2rad(-e.movementY * mouseSensitivity)));
   }
 
   document.addEventListener('pointerlockchange', function(e) {
@@ -62,25 +75,46 @@ window.addEventListener('load', function(e) {
   window.requestAnimationFrame(function anim(now) {
     var dt = prev && (now - prev) / 1000;
     prev = now;
-    if (keyboard.has(65))  // A
-      camera.translate(-1 * dt, 0, 0);
-    if (keyboard.has(68))  // D
-      camera.translate(1 * dt, 0, 0);
-    if (keyboard.has(83))  // S
-      camera.translate(0, 0, 1 * dt);
-    if (keyboard.has(87))  // W
-      camera.translate(0, 0, -1 * dt);
-    var cameraPos = camera.getPos(0, 0, 0);
-    var facingPos = camera.getPos(0, 0, -1);
+
+    if (keyboard.has(65) && !keyboard.has(68)) {  // A
+      pos[0] += Math.sin(yaw - Math.PI / 2) * strafePerSec * dt;
+      pos[2] -= Math.cos(yaw - Math.PI / 2) * strafePerSec * dt;
+    } else if (keyboard.has(68) && !keyboard.has(65)) {  // D
+      pos[0] += Math.sin(yaw + Math.PI / 2) * strafePerSec * dt;
+      pos[2] -= Math.cos(yaw + Math.PI / 2) * strafePerSec * dt;
+    }
+
+    if (keyboard.has(83) && !keyboard.has(87)) {  // S
+      pos[0] -= Math.sin(yaw) * backPerSec * dt;
+      pos[2] += Math.cos(yaw) * backPerSec * dt;
+    } else if (keyboard.has(87) && !keyboard.has(83)) {  // W
+      pos[0] += Math.sin(yaw) * forwardPerSec * dt;
+      pos[2] -= Math.cos(yaw) * forwardPerSec * dt;
+    }
+
     canvasDiv.innerHTML = 'Position: ' +
-        JSON.stringify([Math.round(cameraPos[0] * 10) / 10, Math.round(cameraPos[1] * 10) / 10,
-                        Math.round(cameraPos[2] * 10) / 10]) +
-        '<br>Facing:' +
-        JSON.stringify([Math.round((facingPos[0] - cameraPos[0]) * 10) / 10,
-                        Math.round((facingPos[1] - cameraPos[1]) * 10) / 10,
-                        Math.round((facingPos[2] - cameraPos[2]) * 10) / 10]);
+        JSON.stringify([round(pos[0], 1), round(pos[1], 1), round(pos[2], 1)]) + '<br>' +
+        'Yaw: ' + round(rad2deg(yaw)) + '&deg;<br>' +
+        'Pitch: ' + round(rad2deg(pitch)) + '&deg;<br>' +
+        'Roll: ' + round(rad2deg(roll)) + '&deg;';
     window.requestAnimationFrame(anim);
   });
 });
+
+
+function deg2rad(value) {
+  return Math.PI * value / 180;
+}
+
+
+function rad2deg(value) {
+  return 180 * value / Math.PI;
+}
+
+
+function round(value, digits) {
+  var mult = Math.pow(10, digits || 0);
+  return Math.round(value * mult) / mult;
+}
 
 })();
