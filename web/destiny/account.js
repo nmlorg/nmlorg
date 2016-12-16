85ace1878ff126875719541f648177f97b02d80a
Daniel Reed <n@ml.org>
2016-12-15 23:49:57 -0800

    Start moving back to the native API structures. However, GetAllItemsSummary, for example, returns a data structure like:
    data: {
      characters: [
        {
          characterBase: {
            characterId: "1234"
          }
        }, {
          characterBase: {
            characterId: "5678"
          }
        }
      ],
      items: [
        {
          characterIndex: 0,
          itemId: "23456"
        }, {
          characterIndex: 0,
          itemId: "78901"
        }, {
          characterIndex: 1,
          itemId: "98765"
        }
      ]
    }
    
    which still really needs to be transformed into something like:
    
    data: {
      characters: [
        {
          characterBase: {
            characterId: "1234"
          },
          items: [
            {
              itemId: "23456"
            }, {
              itemId: "78901"
            }
          ]
        }, {
          characterBase: {
            characterId: "5678",
          },
          items: [
            {
              itemId: "98765"
            }
          ]
        }
      ]
    }
    
    in some followup.

--- web/destiny/account.js
+++ web/destiny/account.js
@@ -11,13 +11,44 @@ bungie.Account = class BungieAccount {
   load(membershipType, membershipId) {
     return bungieNetPlatform.userService.GetBungieAccount(membershipId, membershipType)
         .then(data => {
-          this.bungieId = data.Response.bungieNetUser.membershipId;
-          this.bungieName = data.Response.bungieNetUser.displayName;
-          this.accounts = [];
-          for (let account of data.Response.destinyAccounts)
-            this.accounts.push(account);
+          for (let [k, v] of Object.entries(data.Response))
+            this[k] = v;
+          this.destinyAccounts = this.destinyAccounts.map(
+              account => new bungie.DestinyAccount(account));
           return this;
         });
+  }
+};
+
+
+bungie.DestinyAccount = class DestinyAccount {
+  constructor(data) {
+    for (let [k, v] of Object.entries(data))
+      this[k] = v;
+    this.characters = this.characters.map(character => new bungie.DestinyCharacter(character));
+  }
+
+  GetAllItemsSummary() {
+    return bungieNetPlatform.destinyService.GetAllItemsSummary(
+        this.userInfo.membershipType, this.userInfo.membershipId);
+  }
+};
+
+
+bungie.DestinyCharacter = class DestinyCharacter {
+  constructor(data) {
+    for (let [k, v] of Object.entries(data))
+      this[k] = v;
+  }
+
+  GetCharacterInventory() {
+    return bungieNetPlatform.destinyService.GetCharacterInventory(
+        this.membershipType, this.membershipId, this.characterId);
+  }
+
+  GetCharacterInventorySummary() {
+    return bungieNetPlatform.destinyService.GetCharacterInventorySummary(
+        this.membershipType, this.membershipId, this.characterId);
   }
 };
 
